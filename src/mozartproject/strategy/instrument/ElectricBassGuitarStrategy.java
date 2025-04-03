fcdfd1d5b1c501ff735507d6ce5f92c1bed28e0d
OkanDincer <dincerokan29+git@gmail.com>
2025-04-03 16:02:24 -0500

    Strategy Design Pattern.

--- src/mozartproject/strategy/instrument/ElectricBassGuitarStrategy.java
+++ src/mozartproject/strategy/instrument/ElectricBassGuitarStrategy.java
@@ -1,5 +1,13 @@
 package mozartproject.strategy.instrument;
 
-public class ElectricBassGuitarStrategy {
+import javax.sound.midi.Track;
 
+public class ElectricBassGuitarStrategy implements InstrumentStrategy{
+
+	@Override
+	public void applyInstrument(Track track, int channel) throws Exception{
+		InstrumentStrategy.addChange(track, channel, 33); //MIDI instrument Electric Bass Guitar - MIDI 33
+	}
+
+	
 }
--- src/mozartproject/strategy/instrument/TrumpetStrategy.java
+++ src/mozartproject/strategy/instrument/TrumpetStrategy.java
@@ -1,5 +1,13 @@
 package mozartproject.strategy.instrument;
 
-public class TrumpetStrategy {
+import javax.sound.midi.Track;
+
+public class TrumpetStrategy implements InstrumentStrategy{
+	
+	
+	@Override
+	public void applyInstrument(Track track, int channel) throws Exception {
+		InstrumentStrategy.addChange(track, channel, 56); //Trumpet Instrument - MIDI 56
+	}
 
 }
