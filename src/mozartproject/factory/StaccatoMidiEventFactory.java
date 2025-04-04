1652ad415e1024dc3a8f2c8b04c256023995e8e5
apaten0403 <alexispaten@gmail.com>
2025-04-04 01:16:42 -0500

    Created legato and steccato playing style factories

--- src/mozartproject/factory/StaccatoMidiEventFactory.java
+++ src/mozartproject/factory/StaccatoMidiEventFactory.java
@@ -2,19 +2,36 @@ package mozartproject.factory;
 
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiEvent;
+import javax.sound.midi.ShortMessage;
 
 public class StaccatoMidiEventFactory implements MidiEventFactory {
 
+	/**
+	 * 
+	 */
 	@Override
 	public MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException {
-		// TODO Auto-generated method stub
-		return null;
+		ShortMessage noteOn = new ShortMessage();
+		noteOn.setMessage(ShortMessage.NOTE_ON , channel, note, velocity);
+		
+		tick -= 120;
+		
+		MidiEvent newEvent = new MidiEvent(noteOn, tick);
+		return newEvent;
 	}
-
+	
+	/**
+	 * 
+	 */
 	@Override
 	public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
-		// TODO Auto-generated method stub
-		return null;
+		ShortMessage noteOff = new ShortMessage();
+		noteOff.setMessage(ShortMessage.NOTE_OFF , channel, note, 0);
+		
+		tick -= 120;
+		
+		MidiEvent newEvent = new MidiEvent(noteOff, tick);
+		return newEvent;
 	}
 
 }
